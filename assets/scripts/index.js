2610d7ce6060c87d9e75bf72ee7dfdd6049b2966
sjf125 <sjf125@gmail.com>
2016-04-29 16:58:07 -0400

    Flags populating and loading

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,24 +7,23 @@ $(() => {
   events.addHandlers();
 });
 
-console.log("Hello!");
+const displayFlags = function(flags){
+  const template = require('./templates/flag-listing.handlebars');
+  $('.content').append(template({flags}));
+  // console.log(flags);
+};
 
-// const displayFlags = function(flags){
-//   const template = require('./templates/flag-listing.handlebars');
-//   $('.content').append(template({flags}));
-//   // console.log(flags);
-// };
-//
-// const getFlags = function(){
-//   $.ajax({
-//     url: "http://localhost:3000/flags",
-//     // method: 'GET',
-//     // dataType: 'json'
-//   }).done(function(flags){
-//     displayFlags(flags);
-//   });
-// };
+const getFlags = function(){
+  $.ajax({
+    url: "http://localhost:3000/flags",
+    // method: 'GET',
+    // dataType: 'json'
+  }).done(function(data){
+    console.log(data);
+    displayFlags(data.flags);
+  });
+};
 
-// $(document).ready(function(){
-//   getFlags();
-// });
+$(document).ready(function(){
+  getFlags();
+});
