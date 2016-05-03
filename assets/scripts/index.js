248c797aa8f978b5a3204a0d173c4b7b79e01fc5
sjf125 <sjf125@gmail.com>
2016-05-03 11:37:16 -0400

    Separate comment click handlers into separate file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,9 @@
 
 const events = require('./api/events.js');
 const app = require('./api/apiurl.js');
-const authApi = require('./api/ajax.js');
-const authUi = require('./api/ui.js');
+// const authApi = require('./api/ajax.js');
+// const authUi = require('./api/ui.js');
+const comment = require('./comment-click-handlers.js');
 
 require('jquery.lazyload.cjs')($);
 
@@ -76,41 +77,7 @@ const displayComments = function(comments, flagID) {
       $('.flag-comments').append(display({comment}));
     }
   }
-  commentClicks();
-};
-
-const commentClicks = function() {
-  $('#cancel-comment, #new-comment, #edit-comment').on('click', function() {
-    $('#comment-field, #new-comment, #edit-comment').addClass('hidden');
-  });
-  $('#show-comment-field').on('click', function() {
-    $('#comment-field, #new-comment').removeClass('hidden');
-    $('#edit-comment').addClass('hidden');
-    $('#comment').val('');
-  });
-  $('.edit-comment').on('click', function() {
-    $('#comment-field, #edit-comment').removeClass('hidden');
-    let id = $(this).parent().parent().data("id");
-    let content = $(this).parent().parent().find('.comment-content').text();
-    $('#comment').val(content).focus();
-    $('#new-comment').addClass('hidden');
-    $('#edit-comment').data("id", id).removeClass('hidden');
-  });
-  $('.delete-comment').on('click', function() {
-    $(this).addClass('hidden');
-    $(this).parent().find('.delete-confirm').removeClass('hidden');
-  });
-  $('.delete-comment-yes').on('click', function(event) {
-    let id = $(this).data("id");
-    console.log(id);
-    $(this).parent().parent().parent().fadeOut(500);
-    event.preventDefault();
-    authApi.deleteComment(authUi.success, authUi.failure, id);
-  });
-  $('.delete-comment-no').on('click', function() {
-    $(this).parent().addClass('hidden');
-    $(this).parent().parent().find('.delete-comment').removeClass('hidden');
-  });
+  comment.clickHandlers();
 };
 
 $(document).ready(function(){
